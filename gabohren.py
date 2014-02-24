973570b62f8b53b2a107a8b90bfaa0769f82518c
David Eccles (gringer) <github@gringer.org>
2014-02-25 08:33:41 +1300

    Added contrast detect calibration task

--- gabohren.py
+++ gabohren.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from psychopy import core, visual, event, monitors, sound
-import random,math
+import random,math,numpy
 
 # set up monitor resolution
 mon = monitors.Monitor('lenovoTPE', width = 29.4, distance = 50)
@@ -84,83 +84,214 @@ def resetBlobs(showText=True):
     opts['cs'] = random.randint(0,5)
     opts['correctKey'] = keyArr[opts['cs']]
 
-description = visual.TextStim(myWin, text =
-'''In the following exercise your limit of detection for rotation will be discovered. Six
-Gabor patches will appear around the dot at the centre of the screen. Five of
-these will be phasing distractors, and one will be a rotating target (see below).
-The aim of the exercise is to select the rotating target by pressing the correct
-key. The keys that can be pressed are the six keys surrounding D (F,R,E,S,X, 
-and C), and correspond to the patch in the same relative direction. A correct
-response will reduce the rotation speed and produce a beep. An incorrect
-response will increase the rotation speed. After 20 seconds with no response
-the exercise will end.'''.replace("\n"," ") +
-"\n\nPress any key to continue.",
-height = 0.03, pos = (0,0.475), alignVert='top', wrapWidth = 0.8)
 
-nextPhase = False
-example1 = visual.GratingStim(
-        myWin,tex="sin",mask="gauss",texRes=256,
-        opacity = 1, pos = (-0.3, 0), size=0.2, sf=[4,0])
-text1 = visual.TextStim(myWin,pos=(-0.3,-0.2),text='Phasing', height = 0.05,
-    color = 'darkred')
-example2 = visual.GratingStim(
-        myWin,tex="sin",mask="gauss",texRes=256,
-        opacity = 1, pos = (0.3, 0), size=0.2, sf=[4,0])
-text2 = visual.TextStim(myWin,pos=(0.3,-0.2),text='Rotating', height = 0.05,
-    color = 'lightblue')
-while(not nextPhase):
-    example1.setPhase(0.01,'+')
-    example2.setOri(0.5,'+')
-    description.draw()
-    example1.draw()
-    text1.draw()
-    example2.draw()
-    text2.draw()
-    for keys in event.getKeys(timeStamped=True):
-        if keys[0]in ['escape','q']:
-            myWin.close()
-            core.quit()
-        else:
+def task1():
+    description = visual.TextStim(myWin, text =
+    '''In the following exercise your limit of detection for rotation will be discovered. Six
+    Gabor patches will appear around the dot at the centre of the screen. Five of
+    these will be phasing distractors, and one will be a rotating target (see below).
+    The aim of the exercise is to select the rotating target by pressing the correct
+    key. The keys that can be pressed are the six keys surrounding D (F,R,E,S,X, 
+    and C), and correspond to the patch in the same relative direction. A correct
+    response will reduce the rotation speed and produce a beep. An incorrect
+    response will increase the rotation speed. After 20 seconds with no response
+    the exercise will end.'''.replace("\n    "," ") +
+    "\n\nPress any key to continue.",
+    height = 0.03, pos = (0,0.475), alignVert='top', wrapWidth = 0.8)
+    
+    nextPhase = False
+    example1 = visual.GratingStim(
+            myWin,tex="sin",mask="gauss",texRes=256,
+            opacity = 1, pos = (-0.3, 0), size=0.2, sf=[4,0])
+    text1 = visual.TextStim(myWin,pos=(-0.3,-0.2),text='Phasing', height = 0.05,
+        color = 'darkred')
+    example2 = visual.GratingStim(
+            myWin,tex="sin",mask="gauss",texRes=256,
+            opacity = 1, pos = (0.3, 0), size=0.2, sf=[4,0])
+    text2 = visual.TextStim(myWin,pos=(0.3,-0.2),text='Rotating', height = 0.05,
+        color = 'lightblue')
+    while(not nextPhase):
+        example1.setPhase(0.01,'+')
+        example2.setOri(0.5,'+')
+        description.draw()
+        example1.draw()
+        text1.draw()
+        example2.draw()
+        text2.draw()
+        for keys in event.getKeys(timeStamped=True):
+            if keys[0]in ['escape','q']:
+                myWin.close()
+                core.quit()
+            else:
+                nextPhase = True
+        message.draw()
+        myWin.flip()
+    
+    nextPhase = False
+    
+    cMessage = visual.TextStim(myWin, pos = (0.475,-0.475), text="%0.2f" % opts['skill'],
+        height = 0.05, alignVert = 'bottom', alignHoriz = 'right')
+    cMessage.setAutoDraw(True)
+    resetBlobs()
+    trialClock.reset()
+    
+    #repeat drawing for each frame
+    while(not nextPhase):
+        t = float(trialClock.getTime()) / 20
+        logt = (pow(10,t) - 1)/9
+        #stimuli[0].setOpacity(logt)
+        for x in range(6):
+            if(x != opts['cs']):
+                stimuli[x].setPhase(t*math.pi*opts['skill'] + startPhase[x])
+        stimuli[opts['cs']].setOri(180*t*opts['skill']*mdir[cs] + startAngle[cs])
+        message.draw()
+        #handle key presses each frame
+        for keys in event.getKeys(timeStamped=True):
+            if keys[0]in ['escape','q']:
+                myWin.close()
+                core.quit()
+            elif (keys[0] == opts['correctKey']):
+                soundsX[opts['cs']].play()
+                soundsY[opts['cs']].play()
+                trialClock.reset()
+                opts['skill'] *= 0.9
+                resetBlobs(opts['skill'] > 0.5)
+                cMessage.setText("%0.2f" % opts['skill'])
+            elif(keys[0] in keyArr):
+                trialClock.reset()
+                opts['skill'] /= 0.9
+                resetBlobs(opts['skill'] > 0.5)
+                cMessage.setText("%0.2f" % opts['skill'])
+            cMessage.draw()
+        if(trialClock.getTime() >= 20):
             nextPhase = True
-    message.draw()
-    myWin.flip()
+        myWin.flip()
 
-nextPhase = False
+def resetBlobs2(blobArray, rangeDict):
+    n = len(blobArray)
+    poss = random.sample(range(n),n)
+    for x in range(n):
+        mdir[x] = random.choice([-1,1])
+        blob = blobArray[x]
+        p = poss[x]
+        if(x == 0):
+            opts['correctKey'] = keyArr[p]
+            opts['soundX'] = sound.SoundPygame(
+                value = pos2log(blob.pos[0])* 300 + 300)
+            opts['soundX'].setVolume(0.2)
+            opts['soundY'] = sound.SoundPygame(
+                value = pos2log(blob.pos[1])* 300 + 300)
+            opts['soundY'].setVolume(0.2)
+        blob.pos = (math.cos(p*math.pi/(n/2)) * 0.3,
+            math.sin(p*math.pi/(n/2)) * 0.3)
+        for m in rangeDict:
+            if(m == 'ori'):
+                blob.ori = random.uniform(rangeDict[m][0],rangeDict[m][1])
 
-cMessage = visual.TextStim(myWin, pos = (0.475,-0.475), text="%0.2f" % opts['skill'],
-    height = 0.05, alignVert = 'bottom', alignHoriz = 'right')
-cMessage.setAutoDraw(True)
-resetBlobs()
-trialClock.reset()
+def task2():
+    description = visual.TextStim(myWin, text =
+    '''In the following exercise your limit of detection for contrast will be discovered. Six
+    patches will appear around the dot at the centre of the screen. Five of these will
+    be non-Gabor distractors with a triangular carrier wave, and one will be a Gabor
+    target with a sine carrier wave (see below).
+    The aim of the exercise is to select the gabor target by pressing the correct
+    key. The keys that can be pressed are the six keys surrounding D (F,R,E,S,X, 
+    and C), and correspond to the patch in the same relative direction. A correct
+    response will reduce the contrast and produce a beep. An incorrect
+    response will increase the contrast. After 20 seconds with no response
+    the exercise will end.'''.replace("\n    "," ") +
+    "\n\nPress any key to continue.",
+    height = 0.03, pos = (0,0.475), alignVert='top', wrapWidth = 0.8)
+    
+    nextPhase = False
+    example1 = visual.GratingStim(
+            myWin,tex="tri",mask="gauss",texRes=256, ori = 30,
+            opacity = 1, pos = (-0.3, 0), size=0.2, sf=[4,0])
+    text1 = visual.TextStim(myWin,pos=(-0.3,-0.2),text='Distractor', height = 0.05,
+        color = 'darkred')
+    example2 = visual.GratingStim(
+            myWin,tex="sin",mask="gauss",texRes=256, ori = 30,
+            opacity = 1, pos = (0.3, 0), size=0.2, sf=[4,0])
+    text2 = visual.TextStim(myWin,pos=(0.3,-0.2),text='Gabor', height = 0.05,
+        color = 'lightblue')
+        
+    while(not nextPhase):
+        example1.setOri(0.5,'+')
+        example1.setPhase(0.01,'+')
+        example2.setOri(0.5,'+')
+        example2.setPhase(0.01,'+')
+        description.draw()
+        example1.draw()
+        text1.draw()
+        example2.draw()
+        text2.draw()
+        for keys in event.getKeys(timeStamped=True):
+            if keys[0]in ['escape','q']:
+                myWin.close()
+                core.quit()
+            else:
+                nextPhase = True
+        message.draw()
+        myWin.flip()
+    
+    opts['score'] = 0
+    cMessage = visual.TextStim(myWin, pos = (0.475,-0.475),
+        text="%0.3f" % opts['skill'],
+        height = 0.05, alignVert = 'bottom', alignHoriz = 'right')
+    sMessage = visual.TextStim(myWin, pos = (-0.475,-0.475),
+        text="%d" % opts['score'],
+        height = 0.05, alignVert = 'bottom', alignHoriz = 'left')
+    patches = [None] * 6
+    ranges = {'ori': (0,180,'linear')}
+    patches[0] = visual.GratingStim(
+            myWin,tex="sin",mask="gauss",texRes=256, ori = 30,
+            opacity = 1, pos = (0.3, 0), size=0.2, sf=[4,0])
+    for x in range(1,6):
+        patches[x] = visual.GratingStim(
+            myWin,tex="tri",mask="gauss",texRes=256, ori = 30,
+            opacity = 1, pos = (0.3, 0), size=0.2, sf=[4,0])
+    resetBlobs2(patches, ranges)
 
-#repeat drawing for each frame
-while(not nextPhase):
-    t = float(trialClock.getTime()) / 20
-    logt = (pow(10,t) - 1)/9
-    #stimuli[0].setOpacity(logt)
-    for x in range(6):
-        if(x != opts['cs']):
-            stimuli[x].setPhase(t*math.pi*opts['skill'] + startPhase[x])
-    stimuli[opts['cs']].setOri(180*t*opts['skill']*mdir[cs] + startAngle[cs])
-    message.draw()
-    #handle key presses each frame
-    for keys in event.getKeys(timeStamped=True):
-        if keys[0]in ['escape','q']:
-            myWin.close()
-            core.quit()
-        elif (keys[0] == opts['correctKey']):
-            soundsX[opts['cs']].play()
-            soundsY[opts['cs']].play()
-            trialClock.reset()
-            opts['skill'] *= 0.9
-            resetBlobs(opts['skill'] > 0.5)
-            cMessage.setText("%0.2f" % opts['skill'])
-        elif(keys[0] in keyArr):
-            trialClock.reset()
-            opts['skill'] /= 0.9
-            resetBlobs(opts['skill'] > 0.5)
-            cMessage.setText("%0.2f" % opts['skill'])
+    nextPhase = False
+    trialClock.reset()
+    opts['skill'] = 1
+    cMessage.setText("%0.2f" % opts['skill'])
+    while(not nextPhase):
+        t = float(trialClock.getTime()) / 5
+        logt = (pow(10,t) - 1)/9
+        op = (t * opts['skill']) if (t < 1) else opts['skill']
+        for x in range(6):
+            patches[x].setOpacity(op)
+            patches[x].setOri(0.5*mdir[x],'+')
+            patches[x].setPhase(0.01,'+')
+            patches[x].draw()
+            if (opts['skill'] > 0.5):
+                labels[x].draw()
         cMessage.draw()
-    if(trialClock.getTime() >= 20):
-        nextPhase = True
-    myWin.flip()
+        sMessage.draw()
+        for keys in event.getKeys(timeStamped=True):
+            if keys[0]in ['escape','q']:
+                myWin.close()
+                core.quit()
+            elif (keys[0] == opts['correctKey']):
+                opts['skill'] *= 0.9
+                opts['score'] += 1
+                trialClock.reset()
+                resetBlobs2(patches, ranges)
+                sMessage.setText("%d" % opts['score'])
+                cMessage.setText("%0.3f" % opts['skill'])
+                opts['soundX'].play()
+                opts['soundY'].play()
+            elif(keys[0] in keyArr):
+                opts['skill'] /= 0.9
+                opts['score'] -= 2
+                trialClock.reset()
+                resetBlobs2(patches, ranges)
+                sMessage.setText("%d" % opts['score'])
+                cMessage.setText("%0.3f" % opts['skill'])
+        if(trialClock.getTime() >= 20):
+            nextPhase = True
+        myWin.flip()
+
+task1()
+task2()
